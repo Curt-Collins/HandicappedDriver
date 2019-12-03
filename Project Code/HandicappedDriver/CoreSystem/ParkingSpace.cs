5cab142d9def7858d650e381aead7ede30c1a9ab
leif2 <57462245+leif2@users.noreply.github.com>
2019-12-03 15:38:12 -0600

    Create ParkingSpace.cs

---
+++ Project Code/HandicappedDriver/CoreSystem/ParkingSpace.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace HandicappedDriver.CoreSystem
+{
+    public class ParkingSpace
+    {
+        private string locationDescription;
+        private bool occupied;
+        private string locationCoordinates;
+        private int id;
+        private ParkingLot parkingLot;
+
+        public ParkingSpace(int id)
+        {
+            this.id = id;
+        }
+
+        public string GetCoordinates()
+        {
+            return locationCoordinates;
+        }
+
+        public void SetOccupied(bool o)
+        {
+           
+        }
+
+        private void LoadInfo()
+        {
+
+        }
+
+        private void SaveInfo()
+        {
+
+        }
+
+    }
+}
