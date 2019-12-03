465a12ac9960e44c3c4f5aec6ef9b4e19cc350f6
Curt-Collins <55626322+Curt-Collins@users.noreply.github.com>
2019-12-03 15:43:01 -0600

    Begin code inserts

---
+++ Project Code/HandicappedDriver/JSONSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+//using ChoETL;
+//using Newtonsoft.Json;
+using System.Text;
+
+namespace HandicappedDriver
+{
+
+ 
+    public class JSONSerializer
+    {
+
+        public string Serialize(object obj)
+        {
+            return 
+            //return JsonConvert.SerializeObject(obj);
+        }
+
+        public object DeSerialize(String s)
+        {
+            return JsonConvert.DeserializeObject(s);
+            // return JsonConvert.DeserializeObject<DataTable>(s);
+        }
+
+    }
+}
