c4c2ba7169a8842e9b3f1bd642ee107cb68889c1
leif2 <57462245+leif2@users.noreply.github.com>
2019-12-03 23:37:42 -0600

    Update Driver.cs

--- Project Code/HandicappedDriver/CoreSystem/Driver.cs
+++ Project Code/HandicappedDriver/CoreSystem/Driver.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Web.Services;
+using HandicappedDriver.Bridge;
+
 namespace HandicappedDriver.CoreSystem
 {
     public class Driver
@@ -11,14 +14,25 @@ namespace HandicappedDriver.CoreSystem
         private string plateState;
         private string username;
 
+        public Driver()
+        {
+
+        }
+
         public Driver(string u)
         {
             username = u;
         }
 
+
         public Driver(int id)
         {
             this.id = id;
+        }
+
+        public Driver(DriverData d)
+        {
+
         }
 
         public void GetProfile()
@@ -26,9 +40,15 @@ namespace HandicappedDriver.CoreSystem
 
         }
 
-        public void ResetPassword()
+        public void ResetPassword(DriverData driver)
         {
-
+            string password = "";
+            driver.password = password;
+            // driver.UpdateProfile();  this will be implemented by Curt in the DriverData class
+            string message = "Thanks for choosing Handicapped Driver!" + password + "please like us on Facebook.";
+            // the two following lines of code will work once the MailAdapter class and send() method in that class are 
+            // MailAdapter m = new MailAdapter();
+            // m.send(driver, message);
         }
 
         public void ValidatePassword(string p)
@@ -54,5 +74,11 @@ namespace HandicappedDriver.CoreSystem
         {
 
         }
+
+        public void SendMessage(DriverData d1, DriverData d2, string message)
+        {
+            // this will send info to d2 about d1 along with the message from d1
+        }
+
     }
 }
