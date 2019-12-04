c333ca409bededd235a643ad5eafee5e00a2b06a
Curt-Collins <55626322+Curt-Collins@users.noreply.github.com>
2019-12-03 22:47:37 -0600

    DriverData functional

--- Project Code/HandicappedDriver/Bridge/DriverData.cs
+++ Project Code/HandicappedDriver/Bridge/DriverData.cs
@@ -17,27 +17,30 @@ namespace HandicappedDriver.Bridge
         protected void LoadDriver()
         {
             String queryString = "SELECT d.ID, d.FullName, d.LicensePlateNum, " +
-                "d.MobileNumber, d.EMailAddress, d.Password p.State FROM " +
-                "Driver d LEFT OUTER JOIN LicensePlateState ON " +
+                "d.MobileNumber, d.EMailAddress, d.Password, p.State FROM " +
+                "Driver d LEFT OUTER JOIN LicensePlateState p ON " +
                 "d.LicensePlateState_ID = p.ID";
 
-            this.Adapter = new OleDbDataAdapter(queryString, this.Connection);
-            DataTable driver = new DataTable();
+            DataSet ds = new DataSet();
 
             if (Connect())
             {
-                Adapter.Fill(driver, "Driver");
+                this.Adapter = new OleDbDataAdapter(queryString, this.Connection);
+                Adapter.Fill(ds);
+                DataTable driver = ds.Tables[0];
 
                 if (driver.Rows.Count == 1)
                 {
                     this.Id = (int)driver.Rows[0]["ID"];
                     this.fullName = (string)driver.Rows[0]["FullName"];
-                    this.fullName = (string)driver.Rows[0]["LicensePlateNum"];
-                    this.fullName = (string)driver.Rows[0]["State"];
-                    this.fullName = (string)driver.Rows[0]["MobileNumber"];
-                    this.fullName = (string)driver.Rows[0]["EMailAddress"];
-                    this.fullName = (string)driver.Rows[0]["Password"];
+                    this.licensePlateNum = (string)driver.Rows[0]["LicensePlateNum"];
+                    this.licensePlateState = (string)driver.Rows[0]["State"];
+                    this.mobileNumber = (string)driver.Rows[0]["MobileNumber"];
+                    this.eMailAddress = (string)driver.Rows[0]["EMailAddress"];
+                    this.password = (string)driver.Rows[0]["Password"];
                 }
+
+                this.Connection.Close();
 
             }
         }
