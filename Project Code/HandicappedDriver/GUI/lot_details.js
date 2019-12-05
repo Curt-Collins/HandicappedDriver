9137e970e629b82f4967eee2b2c4199a94531fd6
Richu Mathew <richu.a.mathew@my.occc.edu>
2019-12-04 19:15:44 -0600

    First push of the day from Richu. Created JSON object for lot details in lot_details.js. Fixed some stuff on some of the web pages.

---
+++ Project Code/HandicappedDriver/GUI/lot_details.js
@@ -0,0 +1,30 @@
+﻿function generate_lot_details() {
+    //alert("function called");
+    var lotContainer = document.getElementById("spot-info");
+    var htmlString = "<p>";
+    //var result = "revbjh"
+
+    // get the spot info from c# method
+    PageMethod.ViewAvailableSpaces(lot_id, onSucess, onError);
+
+
+    function onSucess(result) {
+        htmlString += result;
+        htmlString += "</p>";
+
+    }
+    function onError(result) {
+        alert("Something went wrong. Please try again");
+    }
+    lotContainer.insertAdjacentHTML('beforeend', htmlString);
+
+}
+
+function create_lot_data() {
+    var space_ID = document.getElementById('myLotDropdown').value;
+    var start_time = document.getElementById('startTime').value;
+    var end_time = document.getElementById('endTime').value;
+
+    var lot_data_object = "{" + "\"space_ID\"" + space_ID + "," + "\"FromTime\"" + start_time + "," + "\"UntilTime\"" + end_time + "}";
+    return lot_data_object;
+}
---
+++ Project Code/HandicappedDriver/GUI/message_driver.js
@@ -0,0 +1,16 @@
+﻿// https://codepedia.info/how-to-call-javascript-function-from-code-behind-server-side-asp-net-c/
+
+function showConfirmationMessage() {
+    alert("Message has been successfull sent.");
+}
+
+
+function getDriverVehicleData() {
+    var regState = document.getElementById('regState').value;
+    var regNum = document.getElementById('regID').value;
+    var msg = document.getElementById('msg').value;
+
+    var driverDataObject = "{" + regState + "," + regNum + "," + msg + "}";
+    return driverDataObject;
+
+}
