9ad21758252076be24d9e165c702652329fbecdc
Curt-Collins <55626322+Curt-Collins@users.noreply.github.com>
2019-12-04 19:03:49 -0600

    Data stuff

--- Project Code/HandicappedDriver/Bridge/LicensePlatesStateData.cs
+++ Project Code/HandicappedDriver/Bridge/LicensePlatesStateData.cs
@@ -1,11 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Data.SqlClient;
+using System.Collections;
 
 namespace HandicappedDriver.Bridge
 {
-    public class LicensePlatesStateData
+    public class State
     {
+        public int id;
+        public string name;
+    }
+
+    public class LicensePlatesStateData : HandicappedDriverTableData
+    {
+        public ArrayList<State> States;
+
+        public LicensePlatesStateData()
+        {
+            string queryString = "";
+            State st;
+
+            if (Connect())
+            {
+                States = new ArrayList<State>();
+
+                SqlCommand cmd = Connection.CreateCommand();
+                cmd.CommandText = queryString;
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    st = new State();
+                    st.id = rdr.GetInt32(0);
+                    st.name = rdr.GetString(1);
+
+                }
+
+                rdr.Close();
+                this.Connection.Close();
+            }
+        }
+
+        public class ArrayList<T>
+        {
+        }
     }
 }
