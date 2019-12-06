b3868a3305b60be23d39b569ab1a3aeacf949f69
Richu Mathew <richu.a.mathew@my.occc.edu>
2019-12-06 02:18:44 -0600

    Update useReservation.js

--- Project Code/HandicappedDriver/GUI/useReservation.js
+++ Project Code/HandicappedDriver/GUI/useReservation.js
@@ -1,19 +1,63 @@
 ﻿// resource(s) for map direction
 // https://jsfiddle.net/vgrem/51nkmncd/
 
-function showMap() {
-    var lat = "35.654066";
-    var lng = "-97.473176";
-    //var lat = "" + latitude;
-    //var lng = "" + longitude;
-    var url = "https://maps.google.com/?q=" + lat + "," + lng;
-    //var url = "https://www.google.com/maps/@" + lat + "," + lng;
-    window.open(url);
+var resv_ID;
+
+$(document).ready(function () {
+    var x = document.cookie;
+    //alert(x);
+    var arr = x.split("=");
+    
+    //for (var i = 0; i < arr.length; i++) {
+    //    console.log(arr[i]);
+    //}
+    var userName = arr[1];
+    //alert(userName);
+
+    PageMethods.ShowExistingReservation(username, onSucess, onError);
+    function onSucess(result) {
+
+        // check "resvID" variable name
+        var resvObj = JSON.parse(result)
+
+        resv_ID = resvObj.resvID;
+        resv_ID += "";
+
+    }
+
+    function onError(result) {
+        alert("Error: loading reservation data");
+    }
+    
+
+});
+
+function fncn_occupy() {
+    PageMethods.OccupySpace(resv_ID, onSucess, onError);
+    function onSucess(result) {
+        alert("Status has been changed to occupied");
+    }
+    function onError(result) {
+        alert("Occupy: Error while changing parking status");
+    }
 }
 
-            // function openGoogleMaps(){
-            //     var cords = "40.4003053,-104.7702377";
-            //     var url = "https://www.google.com/maps/search/?api=1&q"+cords;//"https://www.google.com.sa/maps/search/"+ cords +",12.21z?hl=en";
-            //     windows.open("https://www.google.com.sa/maps/@35.6237254,-97.5573735,15z");
-            //     // window.open("https://www.w3schools.com");
-            // }
+function fncn_leave() {
+    PageMethods.LeaveSpace(resv_ID, onSucess, onError);
+    function onSucess(result) {
+        alert("Status has been changed to available");
+    }
+    function onError(result) {
+        alert("Leave: Error while changing parking status");
+    }
+}
+
+function fncn_CancelReservation() {
+    PageMethods.CancelReservation(resv_ID, onSucess, onError);
+    function onSucess(result) {
+        alert("Cancellation has been sucessfull");
+    }
+    function onError(result) {
+        alert("Cancel: Error while changing parking status");
+    }
+}
