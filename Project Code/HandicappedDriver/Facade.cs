9dd400a9311f34b0ae21022cb266f5d97801c802
Curt-Collins <55626322+Curt-Collins@users.noreply.github.com>
2019-12-05 18:31:08 -0600

    Pushing NewtonSoft instead of .NET for JSON

--- Project Code/HandicappedDriver/Facade.cs
+++ Project Code/HandicappedDriver/Facade.cs
@@ -90,14 +90,14 @@ namespace HandicappedDriver
         public string NavigateToSpace(string spaceID)
         {
             // this pulls up the Navigation system to navigate to the space that the user wants to go to
-            //ParkingSpaceData p = new ParkingSpaceData(spaceID);
+            ParkingSpaceData p = new ParkingSpaceData(spaceID);
             string s = "";
-            //p = jSON.DeSerialize<ParkingSpaceData>(spaceID);
-            //p.LoadInfo();
-            //if(String.IsNullOrEmpty(p.GetNavInfo()) == false) 
-            //{
-            //    s = jSON.Serialize<string>(p.GetNavInfo());
-            //}
+            p = jSON.DeSerialize<ParkingSpaceData>(spaceID);
+            p.LoadInfo();
+            if (String.IsNullOrEmpty(p.GetNavInfo()) == false)
+            {
+                s = jSON.Serialize<string>(p.GetNavInfo());
+            }
             return s;
         }
         
