544d332789684f4e370ecbd72a4f9fc5e1562bd7
leif2 <57462245+leif2@users.noreply.github.com>
2019-12-08 15:14:22 -0600

    Create JSONSerializer.cs

---
+++ HandicappedParking/HandicappedParking/JSONSerializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HandicappedDriver
+{
+    public class JSONSerializer
+    {
+        public string Serialize<T>(T obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public T DeSerialize<T>(String s)
+        {
+            return JsonConvert.DeserializeObject<T>(s);
+        }
+
+    }
+}
