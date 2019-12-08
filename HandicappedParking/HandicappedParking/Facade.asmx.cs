94b63baf8f690fd72c8cf8fc335c38c1190ca87a
Curt-Collins <55626322+Curt-Collins@users.noreply.github.com>
2019-12-07 23:58:09 -0600

    Ugh

--- HandicappedParking/HandicappedParking/Facade.asmx.cs
+++ HandicappedParking/HandicappedParking/Facade.asmx.cs
@@ -2,27 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Script.Services;
 using System.Web.Services;
+using Newtonsoft.Json;
 
 namespace HandicappedParking
 {
     /// <summary>
     /// Summary description for Facade
     /// </summary>
-    ///     [WebService(Namespace = "http://tempuri.org/")]
-
     [WebService(Namespace = "HandicappedParking")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+    [System.Web.Script.Services.ScriptService]
     public class Facade : System.Web.Services.WebService
     {
 
-        [WebMethod]
+        [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = false)]
         public string HelloWorld()
         {
-            return "Hello World";
+            return "[{\"msg\" : \"Hello World\"}]";
+
+        }
+
+        [WebMethod]
+        public static string StatesTest()
+        {
+            return
+                "[" + "" +
+                "{" + "name:" + "Alberta," + "abbreviation: AB" + "}," +
+                "{" + "name:" + "British Columbia," + "abbreviation: BC" + "}," +
+                "{" + "name:" + "Alberta," + "abbreviation: MN" + "}," +
+                "]";
         }
     }
 }
