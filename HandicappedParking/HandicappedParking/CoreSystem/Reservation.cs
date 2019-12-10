7ecc4030187189359679ed3f0d7e45fd251a0c89
Richu Mathew <richu.a.mathew@my.occc.edu>
2019-12-10 16:08:51 -0600

    Create Reservation.cs

---
+++ HandicappedParking/HandicappedParking/CoreSystem/Reservation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using HandicappedDriver.Bridge;
+
+// changed HandicappedDriver to HandicappedParking
+namespace HandicappedParking.CoreSystem
+{
+    public class Reservation
+    {
+        private int id;
+        public DateTime startTime;
+        public DateTime endTime;
+        private ParkingSpace parkingSpace;
+		private DriverData driverData;
+        private Driver driver;
+
+        public Reservation(Driver d, ParkingSpace ps, DateTime startTime, DateTime endTime)
+        {
+            this.startTime = startTime;
+            this.endTime = endTime;
+        }
+
+        public string GetReservationInfo()
+        {
+            return "";
+        }
+
+        public void OccupyParkingSpace(int id)
+        {
+			parkingSpace.SetOccupied(true);
+		}
+
+        public void LeaveParkingSpace()
+        {
+			parkingSpace.SetOccupied(false);
+        }
+
+        public void CancelReservation()
+        {
+			
+		}
+
+        public void MakeReservation()
+        {
+
+        }
+
+        private void LoadInfo()
+        {
+
+        }
+
+        private void SaveInfo()
+        {
+
+        }
+    }
+}
