97e85de15df9c157c3c660817c4b0e8c12e5c9a8
Richu Mathew <richu.a.mathew@my.occc.edu>
2019-12-10 16:08:09 -0600

    Create ParkingLotData.cs

---
+++ HandicappedParking/HandicappedParking/Bridge/ParkingLotData.cs
@@ -0,0 +1,43 @@
+﻿using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace HandicappedDriver.Bridge
+{
+    public class LotInfo
+    {
+        public int id;
+        public string name;
+    }
+
+    public class ParkingLotData : HandicappedDriverTableData
+    {
+        public List<LotInfo> Lots;
+
+        public ParkingLotData()
+        {
+            string queryString = "SELECT id, LotName FROM ParkingLot";
+            LotInfo l;
+
+            if (Connect())
+            {
+                Lots = new List<LotInfo>();
+
+                SqlCommand cmd = Connection.CreateCommand();
+                cmd.CommandText = queryString;
+                SqlDataReader rdr = cmd.ExecuteReader();
+
+
+                while (rdr.Read())
+                {
+                    l = new LotInfo();
+                    l.id = rdr.GetInt32(0);
+                    l.name = rdr.IsDBNull(1) ? "" : rdr.GetString(1);
+                    Lots.Add(l);
+                }
+
+                rdr.Close();
+                this.Connection.Close();
+            }
+        }
+     }
+}
